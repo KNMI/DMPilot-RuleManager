896bff316540ca2a801a69f04adfc51f375f0253
Joao Zanetti <joao.pereirazanetti@knmi.nl>
2019-02-26 09:22:17 +0100

    Rule options docstings, print() to logger.info().

--- rules/__init__.py
+++ rules/__init__.py
@@ -24,6 +24,14 @@ class RuleFunctions():
         """
         Function RuleFunctions::purge
         Handler for the temporary archive purge rule.
+
+        Parameters
+        ----------
+        options : `dict`
+            The rule's options.
+            - ``daysPurgeAfter``: Number of days after which the file should be deleted (`int`)
+        SDSFile : `SDSFile`
+            The file to be processed.
         """
 
         # We can check time modified etc etc..
@@ -31,13 +39,21 @@ class RuleFunctions():
             return
 
         # Some other configurable rules
-        print(irodsSession.purgeTemporaryFile(SDSFile))
+        logger.info("Purging file: " + SDSFile.filename)
+        irodsSession.purgeTemporaryFile(SDSFile)
 
     def psdMetadata(self, options, SDSFile):
         """
         Function RuleFunctions::wfcatalog
         Handler for the WFCatalog metadata rule
         TODO XXX
+
+        Parameters
+        ----------
+        options : `dict`
+            The rule's options.
+        SDSFile : `SDSFile`
+            The file to be processed.
         """
 
         print(psdCollector.process(SDSFile))
@@ -47,16 +63,37 @@ class RuleFunctions():
         Function RuleFunctions::wfcatalog
         Handler for the WFCatalog metadata rule
         TODO XXX
+
+        Parameters
+        ----------
+        options : `dict`
+            The rule's options.
+        SDSFile : `SDSFile`
+            The file to be processed.
         """
         if mongoSession.getMetadataDocument(SDSFile) is not None:
             return
 
-        #print(collector.getMetadata(SDSFile))
+        logger.info(collector.getMetadata(SDSFile))
 
     def ingestion(self, options, SDSFile):
         """
         Function RuleFunctions::ingestion
         Handler for the ingestion rule
+
+        Parameters
+        ----------
+        options : `dict`
+            The rule's options.
+            - ``days``: Maximum age of the file to be ingested (`int`)
+            - ``prune``: Whether or not to prune the file (`bool`)
+            - ``repackRecordSize``: (`int`)
+            - ``daysIgnoreOlderThan``: (`int`)
+            - ``rescName``: Name of the iRODS resource to save the object (`str`)
+            - ``purgeCache``: Whether or not to purge the cache,
+                              in case the resource is compound (`bool`)
+        SDSFile : `SDSFile`
+            The file to be processed.
         """
 
         # Check the modification time of the file
@@ -69,13 +106,13 @@ class RuleFunctions():
 
         # A prune is requested
         if options["prune"]:
-            print("Prune is requested.")
+            logger.info("Prune is requested.")
 
         # Attempt to ingest to iRODS
         irodsSession.createDataObject(SDSFile, rescName="compResc", registerChecksum=True)
 
         # Check if checksum is saved
-        print(irodsSession.getDataObject(SDSFile).checksum)
+        logger.info(irodsSession.getDataObject(SDSFile).checksum)
 
     def isIngested(self, SDSFile):
         """
@@ -86,7 +123,15 @@ class RuleFunctions():
         return False
 
     def dublinCore(self, options, sdsFile):
-        """Process and save Dublin Core metadata of an SDS file."""
+        """Process and save Dublin Core metadata of an SDS file.
+
+        Parameters
+        ----------
+        options : `dict`
+            The rule's options.
+        SDSFile : `SDSFile`
+            The file to be processed.
+        """
 
         if DublinCore.getDCMetadata(sdsFile) is not None:
             logger.info("DC metadata already exists for " + sdsFile.filename)
