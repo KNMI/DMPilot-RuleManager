5d37d28edd6ce71ee5ffbbe974c5e9ce82c9dac4
Mathijs Koymans <m.r.koymans@gmail.com>
2019-05-01 10:55:13 +0000

    Add congifuration.samply.py

--- configuration.sample.py
+++ configuration.sample.py
@@ -1,21 +1,20 @@
 config = {
-    "IRODS_ROOT": "/awsZone/home/rods/",
+    "IRODS_ROOT": "/ZoneA/home/rods/",
     "FDSNWS_ADDRESS": "https://www.orfeus-eu.org/fdsnws/station/1/query",
     "MONGO": {
         "HOST": "localhost",
         "PORT": 27017,
-        "USER": "user",
-        "PASS": "pass",
+        "USER": "username",
+        "PASS": "password",
         "DATABASE": "wfrepo",
         "DC_METADATA_COLLECTION": "wf_do",
         "WF_METADATA_COLLECTION": "daily_streams"
     },
     "IRODS": {
-        "ENABLED": True,
         "HOST": "localhost",
         "PORT": "1247",
-        "USER": "rods",
-        "PASS" : "xxxxxx",
-        "ZONE": "awsZone"
+        "USER": "username",
+        "PASS": "password",
+        "ZONE": "ZoneA"
     }
 }
