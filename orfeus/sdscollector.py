b22d7783f9743bfbd212a8db1d5e79fd60791d6d
Jordi Domingo Ballesta <jdomfb@gmail.com>
2019-05-13 15:19:54 +0000

    Added method to collect files by modification time (instead of julian date)

--- orfeus/sdscollector.py
+++ orfeus/sdscollector.py
@@ -11,6 +11,7 @@
 from fnmatch import fnmatch
 from datetime import datetime, timedelta
 from itertools import repeat
+import dateutil.parser as parser
 
 from orfeus.sdsfile import SDSFile
 from orfeus.filecollector import FileCollector
@@ -38,20 +39,41 @@ class SDSFileCollector(FileCollector):
         Returns all files in the SDS archive
         """
 
-        return map(SDSFile, self.files, repeat(self.archiveDir))
+        return list(map(SDSFile, self.files, repeat(self.archiveDir)))
 
-    def collectFromDate(self, date):
+    def collectFromDate(self, date, mode='file_name'):
         """
         def fileCollector.collectFromDate
-        Collects SDS files for a particular date
+        Collects SDS files for a particular date, based on file's name or on
+        file's modification time
         """
 
-        # Extract the julian day and year
-        day = date.strftime("%j")
-        year = date.strftime("%Y")
+        # Parse provided date if necessary
+        if not isinstance(date, datetime):
+            date = parser.parse(date)
 
-        # Filter by day and year
-        return list(filter(lambda x: (x.day == day and x.year == year), self.files))
+        if mode == 'file_name':
+
+            # Extract the julian day and year
+            day = date.strftime("%j")
+            year = date.strftime("%Y")
+            self.logger.debug("Searching files whose name's date is '%s.%s'" % (year, day))
+
+            # Filter by day and year
+            files = list(filter(lambda x: (x.day == day and x.year == year), self.files))
+
+        elif mode == 'mod_time':
+
+            # Extract start and end of the date
+            date_start = datetime(date.year, date.month, date.day)
+            date_end = date_start + timedelta(days=1)
+            self.logger.debug("Searching files modified between '%s' and '%s'" % (date_start, date_end))
+
+            # Filter by modification time
+            files = list(filter(lambda x: (x.modified >= date_start and x.modified < date_end), self.files))
+
+        self.logger.debug("Found %d files." % len(files))
+        return files
 
     def collectFromWildcards(self, filename):
         """
@@ -63,10 +85,15 @@ class SDSFileCollector(FileCollector):
         if len(filename.split(".")) != 7:
             raise ValueError("An invalid expression was submitted: %s" % filename)
 
-        # Take the basename and map to SDSFile
-        return list(filter(lambda x: fnmatch(x.filename, filename), self.files))
+        self.logger.debug("Searching files whose name fits in '%s'" % filename)
 
-    def collectFromDateRange(self, date, days):
+        # Take the basename and map to SDSFile
+        files = list(filter(lambda x: fnmatch(x.filename, filename), self.files))
+
+        self.logger.debug("Found %d files." % len(files))
+        return files
+
+    def collectFromDateRange(self, date, days, mode='file_name'):
         """
         def collectFromPast
         Collects files from N days in the past
@@ -78,17 +105,17 @@ class SDSFileCollector(FileCollector):
         for day in range(1, abs(days)):
 
             if(days > 0):
-                collectedFiles += self.collectFromDate(date + timedelta(days=day))
+                collectedFiles += self.collectFromDate(date + timedelta(days=day), mode)
             else:
-                collectedFiles += self.collectFromDate(date - timedelta(days=day))
+                collectedFiles += self.collectFromDate(date - timedelta(days=day), mode)
 
         return collectedFiles
 
-    def collectFromPastDays(self, days):
+    def collectFromPastDays(self, days, mode='file_name'):
         """
         def collectFromPast
         Collects files from N days in the past
         """
 
         # Negative days
-        return self.collectFromDateRange(datetime.now(), -days)
+        return self.collectFromDateRange(datetime.now(), -days, mode)
