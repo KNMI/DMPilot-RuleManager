1405a0e497b33163ccc1c25d6ec48cc2517a9a74
Mathijs Koymans <m.r.koymans@gmail.com>
2019-05-14 13:17:20 +0000

    Add JSON schema for rules.json

---
+++ schema/__init__.py
@@ -0,0 +1,53 @@
+JSON_RULE_SCHEMA = {
+	"id": "rule-engine-schema",
+	"$schema": "http://json-schema.org/draft-04/schema#",
+	"title": "JSON Schema for Rule Engine rules",
+	"description": "Schema that validates rules.json",
+	"type": "object",
+	"patternProperties": {
+		"^.*$": {
+			"description": "Rule",
+			"$ref": "#/definitions/rule",
+			"format": "object"
+		}
+
+	},
+	"definitions": {
+		"rule": {
+			"type": "object",
+			"properties": {
+				"functionName": {
+					"type": "string"
+				},
+				"options": {
+					"type": "object"
+				},
+				"conditions": {
+					"type": "array",
+					"$ref": "#/definitions/condition"
+				},
+				"timeout": {
+					"type": "integer"
+				}
+			},
+			"required": ["functionName", "options", "conditions"],
+			"additionalProperties": False
+		},
+		"condition": {
+			"type": "array",
+			"items": {
+				"type": "object",
+				"properties": {
+					"functionName": {
+						"type": "string"
+					},
+					"options": {
+						"type": "object"
+					}
+				},
+				"required": ["functionName", "options"],
+				"additionalProperties": False
+			}
+		}
+	}
+}
