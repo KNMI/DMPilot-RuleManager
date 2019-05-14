79d9f60f7642c5fb06312609ffa9cfb86898110d
Joao Zanetti <joao.pereirazanetti@knmi.nl>
2019-05-14 16:04:50 +0200

    Deleted ingestion/deletion test.

--- test/runtests.py
+++ test/runtests.py
@@ -356,37 +356,6 @@ class TestRuleManager(unittest.TestCase):
 
         map(testSegment, enumerate(result))
 
-    def test_irods_ingestion_and_deletion(self):
-        """Test ingestion and deletion in iRODS archive."""
-
-        sys.path.append(os.path.dirname(CWD))
-        from modules.irodsmanager import irodsSession
-        sys.path.pop()
-        sys.path.append(os.path.join(os.path.dirname(CWD), "rules"))
-        import sdsrules
-        sys.path.pop()
-
-        with patch("orfeus.sdsfile.SDSFile.irodsDirectory", new_callable=PropertyMock) as mock_dir:
-            mock_dir.return_value = os.path.join(self.SDSReal.irodsRoot,
-                                                 "test",
-                                                 self.SDSReal.subDirectory)
-
-            # Make sure file is not there to start with
-            sdsrules.deleteArchiveRule(None, self.SDSReal)
-            self.assertFalse(irodsSession.exists(self.SDSReal))
-
-            # Archive example file
-            sdsrules.ingestionRule(None, self.SDSReal)
-
-            # Check archived file
-            self.assertTrue(irodsSession.exists(self.SDSReal))
-
-            # Delete file from archive
-            sdsrules.deleteArchiveRule(None, self.SDSReal)
-
-            # Check whether file was deleted
-            self.assertFalse(irodsSession.exists(self.SDSReal))
-
 
 if __name__ == '__main__':
     unittest.main()
