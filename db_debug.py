b16c4ce7bce99c6cede4aa6b5e281e0a3a485260
Joao Zanetti <joao.pereirazanetti@knmi.nl>
2019-05-15 12:44:11 +0200

    db_debug.py script to help debug the deletion database.

---
+++ db_debug.py
@@ -0,0 +1,31 @@
+import sqlite3
+import argparse
+
+from configuration import config
+from core.database import deletion_database
+
+if __name__ == "__main__":
+    # Parse command line arguments
+    parser = argparse.ArgumentParser(description="Tool to debug the DeletionDatabase.")
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument("--list", help="Lists the filenames in the table", action="store_true")
+    group.add_argument("--count",
+                       help="Counts how many filenames there are in the table",
+                       action="store_true")
+    group.add_argument("--remove", help="Removes one filename from the table")
+    group.add_argument("--clear", help="Clears the table", action="store_true")
+    parsedargs = parser.parse_args()
+
+    if parsedargs.count:
+        print(len(deletion_database.get_all_files()))
+
+    if parsedargs.list:
+        for sds_file in deletion_database.get_all_files():
+            print(sds_file.filename)
+
+    if parsedargs.clear:
+        for sds_file in deletion_database.get_all_files():
+            deletion_database._delete_row(sds_file.filename)
+
+    if parsedargs.remove is not None:
+        deletion_database._delete_row(parsedargs.remove)
