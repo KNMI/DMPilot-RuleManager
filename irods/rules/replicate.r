2074621f0bff98f19acbe5744825bb2da70b7624
Mathijs Koymans <m.r.koymans@gmail.com>
2019-05-29 10:38:12 +0000

    Add EUDAT iRODS rules

---
+++ irods/rules/replicate.r
@@ -0,0 +1,13 @@
+Replication {
+    *registered = "true";
+    *recursive = "true";
+    *status = EUDATReplication(*source, *destination, *registered, *recursive, *response);
+    if (*status) {
+        writeLine("stdout", "Replica *source on *destination Success!");
+    }
+    else {
+        writeLine("stdout", "Replica *source on *destination Failed: *response");
+    }
+}
+INPUT *source="",*destination=""
+OUTPUT ruleExecOut
